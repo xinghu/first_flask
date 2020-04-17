2832d548446e744be56cda592f720d460d64aad8
hupengxing <hupengxing@hupengxing.com>
2020-04-17 08:23:02 +0800

    [ADD] 增加python dot-env

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from app.views.user import user_page
 from app.extension import db
+import os
+
 
 app = Flask('app')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://tianji:tianji@127.0.0.1/python_admin'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.secret_key = "lalskskskskksksjsj"
